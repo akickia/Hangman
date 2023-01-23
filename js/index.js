3d0c58dfd43c2b4df8b211898d9bb33b9b28c8ad
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-01-23 10:49:58 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/**
+ För att toggla SVG:en
+ document.querySelector('figure').classList.add('scaffold')
+ document.querySelector('figure').classList.add('head')
+ document.querySelector('figure').classList.add('body')
+ document.querySelector('figure').classList.add('arms')
+ document.querySelector('figure').classList.add('legs')
+ */
