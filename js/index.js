e831af1fe6065297bdb4a2afdb7bcaf505423234
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-01-23 10:59:19 +0100

    added comments

--- js/index.js
+++ js/index.js
@@ -6,3 +6,20 @@
  document.querySelector('figure').classList.add('arms')
  document.querySelector('figure').classList.add('legs')
  */
+
+ //- lista med alfabetet
+// - lista med gissade bokstäver
+// - tom lista. För varje gissad bokstav - pusha bokstav till tom lista. 
+// - slumpa ut random ord
+// - Skapa lista med ord och slumpa från listan. 
+// - lista med ord
+//För varje gissad bokstav - om bokstaven finns i ordet så ska bokstaven visas.
+//Else då ändras opaciteten på ett element i svgn. 
+// - max antal gissningar
+// - Om alla delar är visade = Game over
+// - om alla bokstäver är hittade = Vinst 
+// - antal gissningar anv gjort
+
+
+// VG
+// - poängräknare
