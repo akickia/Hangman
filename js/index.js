1c270cd03cb5b061888601467863d871dcd5ccc5
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-01-24 14:36:37 +0100

    added comments

--- js/index.js
+++ js/index.js
@@ -1,59 +1,79 @@
 // ------- VARIABLER ----------
+//Lista random ord
 const words = ["SKOLA", "HEMMA", "VISPGRÄDDE", "UKULELE", "FLAGGSTÅNG", "KLOCKA", "HUMOR" ]
+//Tom lista för gissade bokstäver
 const guessedLetters = []
+//Bokstav i ord
 let wordLetter
+//Knapptryck
 let keypress
-let ulElement = document.querySelector(".word"); 
-let noMatchElement = document.querySelector(".nomatch")
+//Antal felgissningar
 let wrongAnswer = 0
 
-//Kör funktioner
+//Kalla på funktioner
 generateRandomWord()
 displayLetters()
 
 //Lyssna efter tangenttryck
 window.addEventListener("keypress", (event)=>{
+  //konvertera knapptryck till versal
   keypress = event.key.toUpperCase()
   //Om bokstav inte är använd tidigare:
   if (!guessedLetters.includes(keypress)) {
+    //lägg till i listan guessedLetters
     guessedLetters.push(keypress) 
+    //kalla på funktioner
     displayGuessedLetters()
     compareLetters();
   }
-  console.log(guessedLetters)
 })
 
 // ------- FUNKTIONER ----------
 //Generera random ord
 function generateRandomWord() {
   randomWord = words[(Math.floor(Math.random() * (words.length)))]
-  console.log(randomWord)
 }
 
 //Visa antal bokstäver
 function displayLetters () {
+  //hämta ul-elementet "word"
+  let ulElement = document.querySelector(".word"); 
+  //För varje bokstav i ordet
   for (letter in randomWord) {
-    wordLetter = randomWord[letter]
+    //skapa li-element
     let newLiElement = document.createElement("li");
+    //låt wordletter = bokstaven
+    wordLetter = randomWord[letter]
+    //låt texten i li-elementet vara wordletter
     newLiElement.innerText = wordLetter;
+    //lägg till listelementet i ul-elementet
     ulElement.appendChild(newLiElement);
 }}
 
 //Jämför bokstäver och visa om rätt
 function compareLetters() {
+  //hämta li-elementet "word li"
   let liElement = document.querySelectorAll(".word li")
+  //För varje bokstav i ordet
   for (letter in randomWord) {
+    //Om bokstav övensstämmer med knapptryck
     if (randomWord[letter]=== keypress) {
+      //Ändra färg på bokstav så den syns
       liElement[letter].style.color="rgba(0,0,0,1)"
     }
 }
+  //Kalla på funktion
   checkWordForLetter()
 }
 
 //Visa gissade bokstäver
 function displayGuessedLetters () {
+  //Hämta element
+  let noMatchElement = document.querySelector(".nomatch")
   let newLiElement = document.createElement("li");
+  //Sätt li till bokstaven användaren tryckt in
   newLiElement.innerText = keypress;
+  //Lägg till li-element i ul-elementet "nomatch"
   noMatchElement.appendChild(newLiElement);
 }
 
@@ -61,8 +81,11 @@ function displayGuessedLetters () {
 
 //Testa om bokstav finns, annars visa gubbe
 function checkWordForLetter() {
+  //Im bokstaven ej finns i ordet
   if (!randomWord.includes(keypress)) {
+    //Lägg till 1 till variabeln wrongAnswer
     wrongAnswer++
+    //Om wrongAnswer == x - lägg till klass för att visa del av gubbe
     if (wrongAnswer == 1) {
       document.querySelector('figure').classList.add('scaffold')
     }
@@ -75,9 +98,11 @@ function checkWordForLetter() {
     if (wrongAnswer == 4) {
       document.querySelector('figure').classList.add('arms')
     }
+    //Om fel = 5 visa sektionen game-over
     if (wrongAnswer == 5) {
       document.querySelector('figure').classList.add('legs')
       document.querySelector(".game-over").style.display = "flex"
+      //Visa vilket ord vi sökte
       document.querySelector(".game-over p b").innerText = randomWord
     }
   }
